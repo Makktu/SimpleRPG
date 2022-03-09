6aad29f34c797e3f52d62b4415dba134ba20b653
Makktu <johnmcna@gmail.com>
2022-03-09 18:17:12 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Been a while since I did one of my own side-projects. Need the practice.
+
+A simple dungeon-crawling text-based turn-based RPG. Nothing beyond a couple of rooms and a few weapons/monsters for now.
---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const printOut = function (message) {
+    console.log(message);
+};
+
+const timeDelay = function (duration) {
+    // stuff
+};
+
+printOut("Welcome, Adventurer");
+
+timeDelay(5);
+
+printOut("Please enter your name:");

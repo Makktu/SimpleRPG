b49857d3ec1161f5540742533997c982f4e4a8ce
Makktu <johnmcna@gmail.com>
2022-03-09 19:44:56 +0000

    in a tailspin with design

--- script.js
+++ script.js
@@ -1,21 +1,69 @@
 "use strict";
 
-const container = document.querySelector(".container");
-
-const printOut = function (message) {
-    container.innerHTML += `<br>${message}`;
+let character = {
+    charName: "Addy",
+    charLife: 10,
 };
 
-const timeDelay = function (duration) {
-    setTimeout(() => {}, duration);
+let inventory = [
+    {
+        weapon: "sword ⚔️",
+        shield: true,
+        "health potion": 3,
+    },
+];
+
+let enemy1 = {
+    enemyName: "Deformed Goblin",
+    enemyLife: 5,
+};
+let enemy2 = {
+    enemyName: "Cunning Wizard",
+    enemyLife: 10,
+};
+let enemy3 = {
+    enemyName: "Dreadful Ogre 👿",
+    enemyLife: 20,
+};
+
+const container = document.querySelector(".container");
+
+const printOut = function (message, color = "white") {
+    message == "blank"
+        ? (container.innerHTML += "<br><br>")
+        : (container.innerHTML += `<span style="color:${color}"><br>⟫  ${message}</span>`);
+    console.log(message.length);
+};
+
+const timeDelay = function (message) {
+    setTimeout(() => {
+        console.log("begin!");
+        let thisMessage;
+        for (let o = 0; o < message.length; o++) {
+            thisMessage += message.slice(o, o + 1);
+            console.log(thisMessage);
+        }
+    }, 100);
 };
 
 printOut("Welcome, Adventurer! 🏰");
 
-timeDelay(5);
+timeDelay("Testing");
 
+printOut(`Your name is ${character.charName}`, "cyan");
+printOut(`You carry a ${inventory[0].weapon}`, "green");
+inventory[0].shield ? printOut("You also have a shield 🛡", "green") : null;
 printOut(
-    "Do you want to enter a name? (One will be chosen at random for you if you choose No.)"
+    `Neither are very good. You will need better equipment to take on the ${enemy3.enemyName}. In the You Die Arena 🏟`
+);
+printOut(`You have ${character.charLife} ♥️ Life remaining.`, "green");
+printOut(
+    `You are carrying 💊 ${inventory[0]["health potion"]} health ${
+        inventory[0]["health potion"] > 1 ? "packs" : "pack"
+    }.`,
+    "green"
 );
 
-printOut(`<span style="font-size:2rem;">YES / NO</span>`);
+printOut("blank");
+
+printOut("Tap 'BEGIN' to, ah, begin...", "yellow");

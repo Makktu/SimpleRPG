ce3dab1f254158cda5ec07d8a398daede89060f8
Makktu <johnmcna@gmail.com>
2022-03-16 16:12:23 +0000

    return, but stuck on async await things

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 "use strict";
 
-var location = 0;
-
 const mainGame = function () {
-    printOut("The game is afoot!");
-    stateLocation();
+    printOut(introMessage);
+    // printOut("The game is afoot!");
+    // stateLocation();
     giveOptions();
 };
 
@@ -22,14 +21,15 @@ const giveOptions = function () {
 // ********* GAME VARIABLE INITS ******
 // ************************************
 
-const playerLocation = [
-    {
-        place: "in a courtyard at the entrance to the Dreadful Ogre's castle. The iron portcullis is sealed behind you. There is no escape. There are doors to the North, East, and West.",
-        doors: [1, 2, 4],
-    },
-];
+// const playerLocation = [
+//     {
+//         place: "in a courtyard at the entrance to the Dreadful Ogre's castle. The iron portcullis is sealed behind you. There is no escape. There are doors to the North, East, and West.",
+//         doors: [1, 2, 4],
+//     },
+// ];
 
-console.log(playerLocation[0].place);
+// console.log(playerLocation[0].place);
+// let playerLoc = 0;
 
 let okPressed = false;
 const okBtn = document.querySelector(".button1");
@@ -37,10 +37,11 @@ okBtn.addEventListener("click", () => {
     okPressed = true;
 });
 
+// ! currently blocked by browser security feature(s)
 const introTheme = new Audio("sound/adventure.mp3");
 // introTheme.play();
-let nextColor = false;
-let color = "white";
+// ! currently blocked etc
+
 let character = {
     charName: "Addy",
     charLife: 10,
@@ -78,28 +79,33 @@ const container = document.querySelector(".container");
 // ******* THE FAMOUS PRINTOUT FUNCTION ********
 // *********************************************
 
-const printWithDelay = function (charToShow, letter, message) {
+const printWithDelay = function (charToShow, letter) {
     setTimeout(function () {
-        if (charToShow != "|")
-            container.innerHTML += `<span style="color:${color}">${charToShow}</span>`;
+        if (charToShow != "|") container.innerHTML += charToShow;
         if (charToShow == "|") container.innerHTML += `<br>`;
-        console.log(color);
-    }, letter * 15);
+    }, letter * 40);
 };
 
-const printOut = function (message) {
-    // for (let letter = 0; letter < message.length; letter++) {
-    //     if (message[letter] == "|") {
-    //         container.innerHTML += "<br>";
-    //     }
-    //     container.textContent += message[letter];
-    // }
-    container.innerHTML += `<p>${message}</p>`;
-    // container.innerHTML += `<br>`;
-};
+// const printOut = function (message) {
+//     for (let letter = 0; letter < message.length; letter++) {
+//         if (message[letter] == "|") {
+//             container.innerHTML += "<br>";
+//         }
+//         printWithDelay(message[letter], letter);
+//     }
+//     container.innerHTML += `<br>`;
+// };
 
-let introMessage = `Welcome, Adventurer! 🏰 Your name is ${character.charName} | You carry a ${inventory[0].weapon}${inventory[0].weaponIcon} | You also have a shield 🛡️ | Neither are very good. | You will have to find better equipment to take on the ${enemy3.enemyName}...👿 | In the Arena of Death! 💀 | ---------------------------------------------`;
+async function printOut(message) {
+    for (let letter = 0; letter < message.length; letter++) {
+        if (message[letter] == "|") {
+            container.innerHTML += "<br>";
+        }
+        await printWithDelay(message[letter], letter);
+    }
+    container.innerHTML += `<br>`;
+}
 
-printOut(introMessage);
+let introMessage = `Welcome, Adventurer! 🏰 | Your name is ${character.charName} | You carry a ${inventory[0].weapon}${inventory[0].weaponIcon} | You also have a shield 🛡️ | Neither are very good. | You will have to find better equipment to take on the ${enemy3.enemyName}...👿 | In the Arena of Death! 💀 | ---------------------------------------------`;
 
 mainGame();

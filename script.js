9670452050cb8ccfdec02e92d8faccb617bcc19e
Makktu <johnmcna@gmail.com>
2022-04-25 17:34:51 +0100

    1st attempt to get the movement going

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 import { worldLocations, exitNames } from "./world.js";
+import { playerCharacter } from "./player.js";
 
+let btn1Pressed, btn2Pressed, btn3Pressed, btn4Pressed;
+
+// * defines the display area on screen
 const container = document.querySelector(".container");
 
+// * define and assign UI controls (temporary)
+
+const btn1 = document.querySelector(".button1");
+const btn2 = document.querySelector(".button2");
+const btn3 = document.querySelector(".button3");
+const btn4 = document.querySelector(".button4");
+
+btn1.addEventListener("click", () => {
+    // check that the button has an option attached to it
+
+    // if so, implement the choice
+    playerLocation = worldLocations[playerLocation].exits[0];
+    getPlayerMove(worldLocations[playerLocation + 1]);
+});
+btn2.addEventListener("click", () => {
+    playerLocation = worldLocations[playerLocation].exits[1];
+    getPlayerMove(worldLocations[playerLocation + 1]);
+});
+btn3.addEventListener("click", () => {
+    playerLocation = worldLocations[playerLocation].exits[2];
+    getPlayerMove(worldLocations[playerLocation]);
+});
+btn4.addEventListener("click", () => {
+    playerLocation = worldLocations[playerLocation].exits[3];
+    getPlayerMove(worldLocations[playerLocation]);
+});
+
+// * handles output
 const printToScreen = function (message) {
     container.innerHTML += message;
 };
@@ -9,18 +41,17 @@ const printToScreen = function (message) {
 const renderExits = function (exits) {
     let theExits = "<br>";
     for (let exit in exits) {
-        if (exits[exit] == 1) {
+        if (exits[exit]) {
             theExits += `<p class="touchable ${exitNames[exit]}">${exitNames[exit]}</p>`;
         }
     }
     container.innerHTML += `<br><br>There are exits to the ${theExits}`;
 };
 
-const getPlayerMove = function () {
-    let wherePlayer = worldLocations[playerLocation];
-    console.log(wherePlayer.verboseDescription);
+const getPlayerMove = function (wherePlayer) {
+    container.innerHTML = "";
     printToScreen(`You are in ${wherePlayer.placeName}.`);
-    if (worldLocations[0].pickups) {
+    if (worldLocations[playerLocation + 1].pickups) {
         let itemNow = 0;
         for (let item = 0; item < wherePlayer.pickups.length / 2; item++) {
             printToScreen(
@@ -35,13 +66,10 @@ const getPlayerMove = function () {
 
     renderExits(wherePlayer.exits);
     container.innerHTML += "<br><br>What will you do?<br><br>";
-    const northBtn = document.querySelector(".North");
-    northBtn.addEventListener("click", () => {
-        playerLocation = 1;
-        getPlayerMove();
-    });
+
+    // ! janky for now – work on this next
+    // NEED TO MOVE PLAYER
 };
 
-let playerLocation = 0;
-
-getPlayerMove();
+let playerLocation = 1;
+getPlayerMove(worldLocations[playerLocation]);
--- world.js
+++ world.js
@@ -1,19 +1,78 @@
 export let worldLocations = {
-    0: {
+    1: {
         placeName: "the Courtyard of Castle Deth",
-        exits: [1, 1, 1, 1, 1, 0],
+        exits: [4, 2, 0, 3, 0, 0],
         description: "A dusty courtyard at the entrance of Castle Deth.",
         verboseDescription:
             "Nobody would have believed that the courtyard to a place such as Castle Deth could be so ordinary. You could be standing in any courtyard in any castle or mansion in the world. There is nothing to indicate the great Evil that haunts this place. One's flabber is truly gasted.",
         pickups: [],
     },
-    1: {
-        placeName: "the grand hall",
-        exits: [0, 0, 1, 0, 0, 0],
-        description: "The grand hall in Castle Deth.",
+    2: {
+        placeName: "the stables",
+        exits: [0, 0, 0, 1, 0, 0],
+        description: "The stables of Castle Deth.",
         verboseDescription:
-            "The grand hall is empty. The feasting tables and benches are laid as if for a great party. There is only dust and the bones here.",
-        pickups: ["sword", "GOOD", "shield", "AVERAGE"],
+            "The stables. What more to say? There is straw. There is and horse dung. There are saddles hanging from hooks.",
+        pickups: [
+            "horse",
+            "a fast horse that looks as eager to leave Castle Deth as you are",
+        ],
+    },
+    3: {
+        placeName: "the Gardens of Castle Deth",
+        exits: [0, 1, 0, 0, 0, 0],
+        description: "The lovely gardens of Castle Deth.",
+        verboseDescription:
+            "The gardens of Castle Deth are famed for their stench of, well, death.",
+        pickups: [
+            "clothes peg",
+            "the type of wooden clothes peg used to affix washing to a washing line, or to put on somebody's nose in a cartoon (or game...).",
+        ],
+    },
+    4: {
+        placeName: "the Great Hall of Castle Deth",
+        exits: [5, 0, 1, 0, 0, 0],
+        description: "The appropriately-named great hall in Castle Deth.",
+        verboseDescription:
+            "The ceiling of a thousand feet gives the Great Hall its name. The feasting tables and benches are laid as if for a great party. There is only dust and the bones here.",
+        pickups: [
+            "sword",
+            "rusty but in good condition",
+            "shield",
+            "very rusty and in poor condition. Might be more of a hindrance than a help",
+        ],
+    },
+    5: {
+        placeName: "the Feasting Hall of Castle Deth",
+        exits: [6, 0, 4, 0, 0, 1],
+        description: "More feasting?",
+        verboseDescription:
+            "They must have had great times in this place in the day. Yet another feasting hall. Definitely not a lack of imagination on the part of the game designer.",
+        pickups: [],
+    },
+    6: {
+        placeName: "the corridor of Deth",
+        exits: [7, 0, 5, 0, 0, 0],
+        description: "a long, plain corridor in Castle Deth.",
+        verboseDescription:
+            "There are flagstones on the floor and water dripping down the bare brick walls. It's a corridor in a castle. Castle Deth!",
+        pickups: [],
+    },
+    7: {
+        placeName: "the Antechamber of Deth",
+        exits: [8, 0, 6, 0, 0, 0],
+        description: "A room adjacent to Deth itself!",
+        verboseDescription:
+            "Antechamber (<em>an-tee-cheym-ber</em>), noun - a chamber or room that serves as a waiting room and entrance to a larger room or an apartment [<strong>citation needed</strong>",
+        pickups: [],
+    },
+    8: {
+        placeName: "the Throne Room",
+        exits: [0, 0, 0, 0, 0, 0],
+        description: "The throne room of Castle Deth.",
+        verboseDescription:
+            "The Throne Room. You have reached your goal. Can you survive it?",
+        pickups: [],
     },
 };
 
